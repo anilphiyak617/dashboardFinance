7981636acb8518cc87c71d40d2ccafcda4ea400f
Anil Phiyak <phiyakanil@gmail.com>
2023-07-26 15:39:24 +0530

    added customized theme to mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import Card from "@/components/Card"
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
-    <>
-    <Card/>
-      this is the app
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div> dashboard page</div>} />
+              <Route path="/predictions" element={<div> predictions page</div>} />
+            </Routes>
+
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
